cbb5c3a85eae2ef4719c31edbbe76dd5d43837fc
Ennio Caprio <ennio520@gmail.com>
2020-08-14 17:39:29 +0200

    update

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ const ListProduct = require('./routers/ListProduct');
 
 env.config();
 
+app.use(express.json());
+app.use(cors());
+app.use('/api/user', User)
+app.use('/api/product', Product)
+app.use('/api/listProduct', ListProduct)
+
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
@@ -19,13 +25,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const port = process.env.PORT || 5000;
-
-app.use(express.json());
-app.use(cors());
-app.use('/api/user', User)
-app.use('/api/product', Product)
-app.use('/api/listProduct', ListProduct)
-
 
 app.listen(port, () => {
     console.log('Listening on the port:', port)
