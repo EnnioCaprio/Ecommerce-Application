69b1e4bd15e88f022a8ffd7635558e50abf333b5
Ennio Caprio <ennio520@gmail.com>
2020-08-14 18:48:36 +0200

    update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return(
         <div>
             <footer className="footer-main">
-                <h3 className="footer-title">Application developed in 2020 Giggin</h3>
+                <h3 className="footer-title">Application developed in 2020</h3>
             </footer>
         </div>
     )
